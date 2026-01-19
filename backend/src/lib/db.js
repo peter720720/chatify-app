e61fdf0d3a5ed764d1e3c1f54c784ca6d8eee8d9
Akorede <oyinloyepeter273@gmail.com>
2026-01-19 02:12:15 +0100

    signup controller added

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose"
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL)
-        console.log("MONGODG CONNECTED:", conn.connection.host)
+        const { MONGO_URL } = process.env;
+        if (!MONGO_URL) throw new Error("MONGO_URL is not set");
+        
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log("MONGODG CONNECTED:", conn.connection.host);
     } catch (error) {
-        console.error("Error connection to MONGODB:", error)
+        console.error("Error connection to MONGODB:", error);
         process.exit(1); // status code means fail, 0 means success
     }
-}
+};
